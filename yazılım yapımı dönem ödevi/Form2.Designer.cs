599d32d54ba82c2344bad6c83f1d222a2568bdaa
ONUR KAYA <onutkaya@gmail.com>
2022-05-17 23:06:49 +0300

    kronometre eklendi

--- yazılım yapımı dönem ödevi/Form2.Designer.cs
+++ yazılım yapımı dönem ödevi/Form2.Designer.cs
@@ -29,6 +29,7 @@ namespace yazılım_yapımı_dönem_ödevi
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
@@ -39,6 +40,11 @@ namespace yazılım_yapımı_dönem_ödevi
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -136,11 +142,59 @@ namespace yazılım_yapımı_dönem_ödevi
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(683, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(178, 32);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Kalan süre :";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(882, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 24);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "--";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(988, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 24);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "--";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(930, 32);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 32);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "/";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1028, 575);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -171,5 +225,10 @@ namespace yazılım_yapımı_dönem_ödevi
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
--- yazılım yapımı dönem ödevi/Form2.cs
+++ yazılım yapımı dönem ödevi/Form2.cs
@@ -19,9 +19,12 @@ namespace yazılım_yapımı_dönem_ödevi
         
         SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-HVCUDKM;Initial Catalog=yazılım yapım;Integrated Security=True");
         string soruyazisi;
-        
+        int sorusayaci = 0;
+        int saniye = 60;
+        int dakika = 10;
         private void Form2_Load(object sender, EventArgs e)
         {
+            timer1.Start();
             
             //private void sorucek()
             {
@@ -44,22 +47,48 @@ namespace yazılım_yapımı_dönem_ödevi
         
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            //private void sorucek()
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            radioButton4.Checked = false;
+            if (sorusayaci<10)
             {
-                baglanti.Open();
-                SqlCommand komut = new SqlCommand("SELECT top 1 *FROM sorular ORDER BY NEWID()", baglanti);
-                SqlDataReader oku = komut.ExecuteReader();
-                while (oku.Read())
+                sorusayaci++;
+                //private void sorucek()
                 {
-                    label1.Text = soruyazisi = Convert.ToString(oku[1]); // soru
-                    label2.Text = soruyazisi = Convert.ToString(oku[2]); // cevap a
-                    label3.Text = soruyazisi = Convert.ToString(oku[3]); // cevap b
-                    label4.Text = soruyazisi = Convert.ToString(oku[4]); // cevap c
-                    label5.Text = soruyazisi = Convert.ToString(oku[5]); // cevap d
+                    baglanti.Open();
+                    SqlCommand komut = new SqlCommand("SELECT top 1 *FROM sorular ORDER BY NEWID()", baglanti);
+                    SqlDataReader oku = komut.ExecuteReader();
+                    while (oku.Read())
+                    {
+                        label1.Text = soruyazisi = Convert.ToString(oku[1]); // soru
+                        label2.Text = soruyazisi = Convert.ToString(oku[2]); // cevap a
+                        label3.Text = soruyazisi = Convert.ToString(oku[3]); // cevap b
+                        label4.Text = soruyazisi = Convert.ToString(oku[4]); // cevap c
+                        label5.Text = soruyazisi = Convert.ToString(oku[5]); // cevap d
+                    }
+                    baglanti.Close();
+                    //MessageBox.Show(soruyazisi);
                 }
-                baglanti.Close();
-                //MessageBox.Show(soruyazisi);
+            }
+            else
+            {
+                MessageBox.Show("Sınav bitmiştir.");
+                this.Close();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000;
+            saniye = saniye - 1;
+            label8.Text = saniye.ToString();
+            label7.Text = (dakika - 1).ToString();
+            if (saniye == 0)
+            {
+                dakika = dakika - 1;
+                label7.Text = dakika.ToString();
+                saniye = 60;
             }
         }
     }
